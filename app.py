87b8899a05b2d1bc37856c945e3f44f4f417a31f
Juno <DumSpiroSpero@163.com>
2021-02-10 12:08:09 +0800

    0210

--- app.py
+++ app.py
@@ -1,19 +1,26 @@
 from flask import Flask, render_template
 from flask import url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-name = 'Grey Li'
-movies = [{'title': 'My Neighbor Totoro', 'year': '1988'},
-          {'title': 'Dead Poets Society', 'year': '1989'},
-          {'title': 'A Perfect World', 'year': '1993'},
-          {'title': 'Leon', 'year': '1994'},
-          {'title': 'Mahjong', 'year': '1996'},
-          {'title': 'Swallowtail Butterfly', 'year': '1996'},
-          {'title': 'King of Comedy', 'year': '1999'},
-          {'title': 'Devils on the Doorstep', 'year': '1999'},
-          {'title': 'WALL-E', 'year': '2008'},
-          {'title': 'The Pork of Music', 'year': '2012'},]
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:xsSZD0420@localhost:3306/watchlist?utf8mb4'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
